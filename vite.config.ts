b1d6e1ccda85e08bd6c2a62fcf893b6270a3a650
Ducnd <ducnd@inet.vn>
2026-03-09 10:33:54 +0700

    feat: Configure Tauri and Vite with updated app version, identifier, dev server settings, and Linux bundle targets.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
   server: {
     port: 3001,
-    open: true,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 3002,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
   },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
 });
